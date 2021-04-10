7b1174fcc64311c5885c851d0719108a362d97d9
Faustino <fpannunzio@itba.edu.ar>
2021-04-10 19:25:30 -0300

    Los perceptrones reciben todo de config

--- TP3/perceptron.py
+++ TP3/perceptron.py
@@ -11,22 +11,29 @@ from TP3.config import Param, Config
 Function = Callable[[float], float]
 
 
+def calculate_heavy_sum(x: np.ndarray, w: np.array) -> float:
+    return np.sum(x * w)
+
+
 class Perceptron:
-    def __init__(self, l_rate: float) -> None:
+    function_map: Dict[str, Function] = {
+        'sign': np.sign
+    }
+
+    def __init__(self, params: Param) -> None:
         super().__init__()
-        self.l_rate = l_rate
-        self.activation_func = np.sign
+        self._validate_params(params)
+        self.l_rate = params['learning_rate']
+        self.activation_func = self.function_map[params['function']]
+        self.w_min: np.array = None
 
-    def heavy_sum(self, x: np.ndarray, w: np.array) -> float:
-        return np.sum(x * w)
-
-    def calculate_delta(self, x: np.ndarray, y: float, ac: float) -> np.ndarray:
-        return x * self.l_rate * (y - ac)
+    def calculate_delta(self, x: np.ndarray, y: float, heavy_sum: float) -> np.ndarray:
+        return x * self.l_rate * (y - self.activation_func(heavy_sum))
 
     def generate_hyperplane_coefficients(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
         x = np.insert(x, 0, 1, axis=1)
         w: np.array = np.random.rand(len(x[0])) * 2 - 1
-        w_min: np.ndarray = np.copy(w)
+        self.w_min: np.ndarray = np.copy(w)
         error: float = 1.0
         error_min: float = len(x) * 2
         i: int = 0
@@ -37,25 +44,54 @@ class Perceptron:
                 w = np.random.rand(len(x[0])) * 2 - 1
                 n = 0
             i_x: int = random.randint(0, len(x) - 1)
-            ac: float = self.activation_func(self.heavy_sum(x[i_x], w))
-            w += self.calculate_delta(x[i_x], y[i_x], ac)
+            w += self.calculate_delta(x[i_x], y[i_x], calculate_heavy_sum(x[i_x], w))
             error = self.calculate_error(x, y, w)
 
             if error < error_min:
                 error_min = error
-                w_min = np.copy(w)
+                self.w_min = np.copy(w)
             i += 1
 
         return w
 
     def are_validate_coefficients_valid(self, x: np.ndarray, y: np.ndarray) -> bool:
+        x = np.insert(x, 0, 1, axis=1)
         for i in range(len(x)):
-            if not math.isclose(self.activation_func(self.heavy_sum(x[i], self.w_min)), y[i]):
+            if not math.isclose(self.activation_func(calculate_heavy_sum(x[i], self.w_min)), y[i]):
                 return False
         return True
 
     def calculate_error(self, x: np.ndarray, y: np.ndarray, w: np.ndarray) -> float:
-        return sum([0.5 * (abs(y[j] - self.activation_func(self.heavy_sum(x[j], w)))) ** 2 for j in range(len(x))])
+        return sum(abs(y[j] - self.activation_func(calculate_heavy_sum(x[j], w))) for j in range(len(x)))
+
+    def _validate_params(self, params):
+        pass
+
+
+class LinearPerceptron(Perceptron):
+    function_map: Dict[str, Function] = {
+        'linear': lambda var: var,
+    }
+
+    def calculate_error(self, x: np.ndarray, y: np.ndarray, w: np.ndarray) -> float:
+        return sum([0.5 * (abs(y[j] - self.activation_func(calculate_heavy_sum(x[j], w)))) ** 2 for j in range(len(x))])
+
+
+class NonLinearPerceptron(LinearPerceptron):
+    function_map: Dict[str, Function] = {
+        'tanh': np.tanh,
+    }
+
+    derivative_map: Dict[str, Function] = {
+        'tanh': lambda var: 1 - np.tanh(var) ** 2
+    }
+
+    def __init__(self, params: Param) -> None:
+        super().__init__(params)
+        self.derivative = self.derivative_map[params['function']]
+
+    def calculate_delta(self, x: np.ndarray, y: float, heavy_sum: float) -> np.ndarray:
+        return super().calculate_delta(x, y, heavy_sum) * self.derivative(heavy_sum)
 
 
 def _validate_perceptron(perceptron_params: Param) -> Param:
@@ -74,6 +110,5 @@ def get_perceptron(perceptron_params: Param) -> Perceptron:
     _validate_perceptron(perceptron_params)
 
     p_type: Type[Perceptron] = _perceptron_map[perceptron_params['type']]
-    learning_rate = perceptron_params['learning_rate']
 
-    return p_type(learning_rate)
+    return p_type(perceptron_params)
