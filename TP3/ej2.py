594006e5731b3dcc55591faf1fa0294cb6ea453d
isagues <isagues@itba.edu.ar>
2021-04-26 22:40:57 -0300

    Rotulado de graficos en ej2

--- TP3/ej2.py
+++ TP3/ej2.py
@@ -1,98 +1,20 @@
 import math
 import sys
-from typing import Dict, List
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 from plot import lighten_color
 from config import Param, Config
-from config_to_network import get_neural_network, get_neural_network_factory, get_training_set
-from exercises_utils import generate_config
+from config_to_network import get_neural_network_factory, get_training_set
 from neural_network_lib.neural_network import NeuralNetwork
 from neural_network_lib.neural_network_utils import NeuralNetworkFactory, CrossValidationResult, cross_validation
-
-
-class EJ2:
-
-    def __init__(self) -> None:
-        self.training_points: np.ndarray = get_training_set('trainingset/inputs/Ej2.tsv', 1, True)
-        self.training_values: np.ndarray = get_training_set('trainingset/outputs/Ej2.tsv', 1, True)
-
-        self.predictions: Dict[str, Dict[str, List[float]]] = {}
-        self.min_error: List[float] = []
-        self.last_error: List[float] = []
-
-        self.network_params: Param = generate_config()
-
-    def get_network_error(self, network: NeuralNetwork, selected_training_point: int) -> None:
-        self.min_error.append(network.error)
-        self.last_error.append(network.last_training_error)
-
-    def linear(self):
-        self.network_params['type'] = 'linear'
-        neural_network: NeuralNetwork
-
-        self.predictions['linear'] = {}
-
-        # Small
-        self.network_params['learning_rate_strategy'] = 'fixed'
-        self.network_params['base_learning_rate'] = 0.00001
-        # self.network_params['variable_learning_rate_params']['up_scaling_factor'] = \
-        #     self.network_params['variable_learning_rate_params']['down_scaling_factor'] \
-        #     * self.network_params['base_learning_rate']
-        neural_network = get_neural_network(self.network_params, len(self.training_points[0]))
-        self.min_error = []
-        self.last_error = []
-
-        neural_network.train(self.training_points, self.training_values, self.get_network_error)
-
-        self.predictions['linear']['small_min'] = self.min_error
-        self.predictions['linear']['small_last'] = self.last_error
-
-        # Medium
-        self.network_params['learning_rate_strategy'] = 'fixed'
-        self.network_params['base_learning_rate'] = 0.05
-        neural_network = get_neural_network(self.network_params, len(self.training_points[0]))
-        self.min_error = []
-        self.last_error = []
-
-        neural_network.train(self.training_points, self.training_values, self.get_network_error)
-
-        self.predictions['linear']['medium_min'] = self.min_error
-        self.predictions['linear']['medium_last'] = self.last_error
-
-        # Big
-        self.network_params['learning_rate_strategy'] = 'fixed'
-        self.network_params['base_learning_rate'] = 0.8
-        neural_network = get_neural_network(self.network_params, len(self.training_points[0]))
-        self.min_error = []
-        self.last_error = []
-
-        neural_network.train(self.training_points, self.training_values, self.get_network_error)
-
-        self.predictions['linear']['big_min'] = self.min_error
-        self.predictions['linear']['big_last'] = self.last_error
-
-        plt.plot(self.predictions['linear']['big_last'], color=lighten_color('g', 0.3),  label='big_last')
-        plt.plot(self.predictions['linear']['medium_last'], color=lighten_color('m', 0.3), label='medium_last')
-        plt.plot(self.predictions['linear']['small_last'], color=lighten_color('k', 0.3), label='small_last')
-        plt.plot(self.predictions['linear']['big_min'], 'g-', label='big_min', lw=2)
-        plt.plot(self.predictions['linear']['medium_min'], 'm-', label='medium_min', lw=2)
-        plt.plot(self.predictions['linear']['small_min'], 'k-', label='small_min', lw=2)
-        plt.legend()
-
-        plt.semilogy()
-        plt.title('loss per model - momentum: 0.5')
-        plt.ylabel('loss')
-        plt.xlabel('epoch')
-        plt.show()
 
 
 def ej2(config_file: str):
 
     PARTITIONS_COUNT: int = 10
-    ROUNDS: int = 20
+    ROUNDS: int = 5
 
     print(f'Loading config file {config_file}...')
     config: Config = Config(config_file)
@@ -118,44 +40,32 @@ def ej2(config_file: str):
 
     validation_result: CrossValidationResult = cross_validation(neural_network_factory, training_points, training_values,
                                                                 error_metric, len(training_points)//PARTITIONS_COUNT, ROUNDS, metric_comparator=error_comparator, status_callback=save_errors)
+
+    print(f'STD: {validation_result.metrics_std}')
+    print(f'Mean: {validation_result.metrics_mean}')
+    print(f'Best: {validation_result.best_metric}')
+
     if config.plot:
-        plt.figure(figsize=(16, 10))
 
         for error in error_history:
             plt.plot(error, color=lighten_color('g', 0.3))
 
-        plt.plot(np.mean(error_history, axis=0), color='g', lw=3)
-        plt.plot(error_history[validation_result.best_partition_index], color='k', lw=3)
+        l_mean, = plt.plot(np.mean(error_history, axis=0), color='g', lw=3)
+        l_best, = plt.plot(error_history[validation_result.best_partition_index], color='k', lw=3)
+        plt.xlabel("Iteracion ")
+        plt.ylabel("Error")
+        plt.title(f'Error en el entrenamiento. Perceptron {config.network["type"]}. \n'
+                  f'Error {config.network["network_params"]["error_function"]}. Cantidad de particiones {PARTITIONS_COUNT}.')
         plt.semilogy()
-        plt.show()
-
-        plt.boxplot(validation_result.all_metrics)
+        plt.legend([l_mean, l_best], ['Medio', 'Mejor métrica asociada'])
         plt.show()
 
         plt.scatter(error_history[:, -1], validation_result.all_metrics)
-        # plt.plot(np.unique(error_history[:, -1]), np.poly1d(np.polyfit(error_history[:, -1], validation_result.all_metrics, 1))(np.unique(error_history[:, -1])), color='k', lw=3)
+        plt.xlabel("Error en el entrenamiento")
+        plt.ylabel("Metrica del conjunto de validacion (error)")
+        plt.title(f'Error vs Metrica. Perceptron {config.network["type"]}. Iteraciones {config.network["max_training_iterations"]}\n'
+                  f'Error {config.network["network_params"]["error_function"]}. Cantidad de particiones {PARTITIONS_COUNT}.')
         plt.show()
-
-    # best_neural_network: NeuralNetwork = validation_result.best_neural_network
-    #
-    # best_points: np.ndarray = validation_result.best_test_points
-    # best_values: np.ndarray = validation_result.best_test_values
-    #
-    # error_count: int = len(best_neural_network.validate_points(best_points, best_values, error_tolerance=0.001))
-    # close_count: int = len(best_values) - error_count
-    #
-    # print(f'Correct Values: {close_count}\nError Values:{error_count}')
-    #
-    # neural_network2: NeuralNetwork = neural_network_factory()
-    #
-    # errors_over_iter: List[float] = []
-    #
-    # def error_over_iterations(neural_network: NeuralNetwork, point: int):
-    #     errors_over_iter.append(neural_network.error)
-    #
-    # neural_network2.train(best_points, best_values, error_over_iterations)
-    #
-    # plot_error(errors_over_iter)
 
 
 if __name__ == '__main__':
